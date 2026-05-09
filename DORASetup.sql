4a5acc130f8b5f210b37145b5fc62150a710172b
SNOWFLAKE_BADGE5_WORKSHOP <rutujaakadam1610@gmail.com>
2026-05-09 02:13:34 +0000

    DORA Setup Initiated!

---
+++ DORASetup.sql
@@ -0,0 +1,9 @@
+use role accountadmin;
+
+select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123 ) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+);
